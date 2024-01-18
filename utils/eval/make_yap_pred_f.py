55344e5696a0986a0c2ce9f96027cda1791523b3
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-01-18 18:06:35 +0000

    yap morphological parse

---
+++ utils/eval/make_yap_pred_f.py
@@ -0,0 +1,15 @@
+import utils.ner as ner
+import utils.yap as yap
+from utils.eval.consts import TOK
+
+if __name__ == '__main__':
+    tok = ner.read_file_to_sentences_df(TOK)
+        
+    tok_str = ner.raw_toks_str_from_ner_df(tok)
+
+    _, md = yap.yap_joint_api(tok_str)
+
+
+    with open('utils_eval_files/yap_morph_dev.txt', 'w') as w:
+        w.write('\n\n'.join(md.groupby('SENTNUM')['FORM'].agg('\n'.join)))
+
