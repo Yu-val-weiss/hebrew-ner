907d8f13579d6382bd37a4a9b2fe93b4b6064a2e
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-01-18 18:05:30 +0000

    eval scripts

---
+++ utils/eval/consts.py
@@ -0,0 +1,3 @@
+MORPH = "/Users/yuval/GitHub/NEMO-Corpus/data/spmrl/gold/morph_gold_dev.bmes"
+MULTI = "/Users/yuval/GitHub/NEMO-Corpus/data/spmrl/gold/token-multi_gold_dev.bmes"
+TOK = "/Users/yuval/GitHub/NEMO-Corpus/data/spmrl/gold/token-single_gold_dev.bmes"
