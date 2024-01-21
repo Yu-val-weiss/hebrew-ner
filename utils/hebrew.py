3328f94eeb5833be81946c2c31897e9b4c67004a
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-01-21 17:20:46 +0000

    Create hebrew.py

---
+++ utils/hebrew.py
@@ -0,0 +1,13 @@
+def quotes_to_gershayim(s: str): 
+    '''
+    Hebrew can sometimes use gershayim, ״ when meaning to use quotes, especially for acronyms.
+    Yap seems to accept these better.
+    '''
+    return s.replace('"', '״')
+
+def gershayim_to_quotes(s: str):
+    '''
+    Hebrew can sometimes use gershayim, ״ when meaning to use quotes, especially for acronyms.
+    This function normalises backs to quotes.
+    '''
+    return s.replace('״', '"')
