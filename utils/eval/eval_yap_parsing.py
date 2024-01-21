fd44e40ff8967bfe4e971c12eba8442a4274c49a
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-01-21 17:19:53 +0000

    fallback

--- utils/eval/eval_yap_parsing.py
+++ utils/eval/eval_yap_parsing.py
@@ -47,6 +47,19 @@ if __name__ == '__main__':
             
     print("Hybrid pred:", corr/tot)  
     
+    yap = ner.read_file_to_sentences_df('/Users/yuval/GitHub/hebrew-ner/utils_eval_files/yap_hybrid_pred_multi_fallback_dev.txt').groupby('SentNum')['Word'].agg(list)
+    
+    tot = 0
+    corr = 0
+    
+    for gs, hs in zip(gold, yap):
+        for g, h in zip(gs, hs):
+            if g == h:
+                corr += 1
+            tot += 1
+            
+    print("Pred with fallback:", corr/tot) 
+    
     
     yap = ner.read_file_to_sentences_df('/Users/yuval/GitHub/hebrew-ner/utils_eval_files/yap_morph_dev.txt').groupby('SentNum')['Word'].agg(list)
     
