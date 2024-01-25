e0a1fe45d7171cbccf409fba0a450b978bd46e6c
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-01-25 17:27:14 +0000

    morph ftam
    outperforms original morph

---
+++ utils/eval/eval_morph_ftam.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from utils.eval.consts import MORPH, MULTI, TOK
+from utils import ner
+
+if __name__ == '__main__':
+    PRED_MORPH = '/Users/yuval/GitHub/hebrew-ner/ncrf_results/morph/from_hpc_seed46_ftam/results.txt'
+    morph, pred_morph = ner.read_file_to_sentences_df(MORPH), ner.read_file_to_sentences_df(PRED_MORPH)
+    tok, multi = ner.read_file_to_sentences_df(TOK), ner.read_file_to_sentences_df(MULTI)
+    print('GOLD MORPH')
+    ner.evaluate_morpheme(pred_morph, morph, multi, tok)
+    
+    
+    print('\n\nYAP MORPH')
+    
+    tok_grouped = tok.groupby('SentNum')['Label'].agg(list).to_list()
+    
+    print('PURE YAP')
+    
+    ORIGINS = '/Users/yuval/GitHub/hebrew-ner/utils_eval_files/yap_morph_dev_tokens.txt'
+    YAP_MORPH = '/Users/yuval/GitHub/hebrew-ner/ncrf_results/morph/from_hpc_seed46_ftam/yap_results.txt'
+    
+    yap_morph = ner.read_file_to_sentences_df(YAP_MORPH)
+    origins = ner.read_token_origins_to_df(ORIGINS)
+    
+    merged = ner.merge_morph_from_token_origins(yap_morph, origins, validate_to_single=True).groupby('SentNum')['Label'].agg(list).to_list()
+
+    ner.evaluate_token_ner_nested(merged, tok_grouped)
+    
+    print('\nHYBRID + PRED MULTI')
+    
+    yap_morph = ner.read_file_to_sentences_df('/Users/yuval/GitHub/hebrew-ner/ncrf_results/morph/from_hpc_seed46_ftam/hybrid_pred_results.txt')
+    origins = ner.read_token_origins_to_df('/Users/yuval/GitHub/hebrew-ner/utils_eval_files/yap_hybrid_pred_multi_dev_tokens.txt')
+    
+    merged = ner.merge_morph_from_token_origins(yap_morph, origins, validate_to_single=True).groupby('SentNum')['Label'].agg(list).to_list()
+
+    ner.evaluate_token_ner_nested(merged, tok_grouped)
+    
+    print('\nHYBRID + GOLD MULTI')
+    
+    yap_morph = ner.read_file_to_sentences_df('/Users/yuval/GitHub/hebrew-ner/ncrf_results/morph/from_hpc_seed46_ftam/hybrid_gold_results.txt')
+    origins = ner.read_token_origins_to_df('/Users/yuval/GitHub/hebrew-ner/utils_eval_files/yap_hybrid_gold_multi_dev_tokens.txt')
+    
+    merged = ner.merge_morph_from_token_origins(yap_morph, origins, validate_to_single=True)
+    
+    merged = merged.groupby('SentNum')['Label'].agg(list).to_list()
+
+    ner.evaluate_token_ner_nested(merged, tok_grouped)
