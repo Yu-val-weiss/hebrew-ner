bb1f4872afa1c1b6677c510782677f6eea93afee
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-01-31 17:45:30 +0000

    more eval stuff

--- utils/eval/eval_multi.py
+++ utils/eval/eval_multi.py
@@ -2,12 +2,14 @@ from utils import ner
 from utils.eval import consts
 
 
-if __name__ == '__main__':
-
+def eval_multi():
     pred_multi = ner.read_file_to_sentences_df('hpc_eval_results/tok_multi_cnn.txt')
     
     print('Multi to single')
     
     single = ner.read_file_to_sentences_df(consts.TOK)
 
-    ner.evaluate_token_ner(pred_multi['Label'].to_list(), single['Label'].to_list(), multi_tok=True)
+    return ner.evaluate_token_ner(pred_multi['Label'].to_list(), single['Label'].to_list(), multi_tok=True)
+
+if __name__ == '__main__':
+    eval_multi()
