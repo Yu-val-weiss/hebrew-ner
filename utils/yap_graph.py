81fa95b50ed6daa305e481fea67cbad085a83e9b
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-03-27 11:07:28 +0000

    move prune lattices to yap_graph

--- utils/yap_graph.py
+++ utils/yap_graph.py
@@ -2,9 +2,11 @@
 # @Author: me
 import copy
 import math
-from typing import Dict, List, NamedTuple, Union
+from typing import Dict, List, NamedTuple, Set, Tuple, Union
 
 import pandas as pd
+
+from utils import ner
 
 class Edge(NamedTuple):
     From: int
@@ -79,3 +81,25 @@ class YapGraph:
                 # backtrack
                 s.pop()
                 path.pop()
+                
+
+def prune_lattices(lattice_df: pd.DataFrame, multi_df: pd.DataFrame, multi_label_delim='^', fallback=False):
+    splitting = ner.make_multi_splitting_df(multi_df, multi_label_delim)
+    valid_edges: Set[Tuple[int, int, int, int]] = set()
+    for ((sent_id, tok_id), sub_lattice),(_, split) in zip(lattice_df.groupby(['SENTNUM', 'TOKEN']), splitting.groupby(['SentNum', 'WordIndex'])):
+        # tok_id in lattice starts from 1, so may need to offset
+        g = YapGraph.from_df(sub_lattice)
+        source, target = sub_lattice['FROM'].iat[0], sub_lattice['TO'].iat[-1]
+        path_len = split['Splitting'].iat[0] + 1
+        paths = list(g.get_all_paths(source, target, limit=path_len))
+        pruned_paths = [p for p in paths if len(p) == path_len]
+        if fallback and len(pruned_paths) == 0:
+            pruned_paths = [p for p in paths if abs(len(p) - path_len) <= 1]
+        if len(pruned_paths) > 0:
+            paths = pruned_paths
+        for p in paths:
+            for f, t in zip(p[:-1], p[1:]):
+                valid_edges.add((sent_id, tok_id, f, t))
+                
+    cols_to_filter = ['SENTNUM', 'TOKEN', 'FROM', 'TO']
+    return lattice_df[lattice_df[cols_to_filter].apply(tuple, axis=1).isin(valid_edges)].reset_index(drop=True)
