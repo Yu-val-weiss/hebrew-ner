b91912d16b912fe330a1c9090b8cea6e695b3a88
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-04-10 23:07:33 +0100

    morph trn eval hyb

--- utils/eval/eval_trn_morph.py
+++ utils/eval/eval_trn_morph.py
@@ -65,5 +65,51 @@ def eval_all_trn_morph_dev():
     
     return gold_morph_to_morph, gold_morph, pure_yap, pred_multi, gold_multi
 
+def eval_all_trn_morph_test():
+    PRED_MORPH = f'{ENV.ABSOLUTE_PATH_HEBREW_NER}/ncrf_results/transformer/morph/test-results.txt'
+    morph, pred_morph = ner.read_file_to_sentences_df(TEST.MORPH), ner.read_file_to_sentences_df(PRED_MORPH)
+    tok, multi = ner.read_file_to_sentences_df(TEST.TOK), ner.read_file_to_sentences_df(TEST.MULTI)
+    print('GOLD MORPH')
+    gold_morph_to_morph, gold_morph = ner.evaluate_morpheme(pred_morph, morph, multi, tok)
+    
+    
+    print('\n\nYAP MORPH')
+    
+    tok_grouped = tok.groupby('SentNum')['Label'].agg(list).to_list()
+    
+    print('PURE YAP')
+    
+    ORIGINS = f'{ENV.ABSOLUTE_PATH_HEBREW_NER}/utils_eval_files/yap_morph_test_tokens.txt'
+    YAP_MORPH = f'{ENV.ABSOLUTE_PATH_HEBREW_NER}/ncrf_results/transformer/morph/test-yap_morph_results.txt'
+    
+    yap_morph = ner.read_file_to_sentences_df(YAP_MORPH)
+    origins = ner.read_token_origins_to_df(ORIGINS)
+    
+    merged = ner.merge_morph_from_token_origins(yap_morph, origins, validate_to_single=True).groupby('SentNum')['Label'].agg(list).to_list()
+
+    pure_yap = ner.evaluate_token_ner_nested(merged, tok_grouped)
+    
+    print('\nHYBRID + PRED MULTI')
+    
+    yap_morph = ner.read_file_to_sentences_df(f'{ENV.ABSOLUTE_PATH_HEBREW_NER}/ncrf_results/transformer/morph/test-pred_multi_results.txt')
+    origins = ner.read_token_origins_to_df(f'{ENV.ABSOLUTE_PATH_HEBREW_NER}/utils_eval_files/yap_hybrid_trn_pred_multi_test_tokens.txt')
+    
+    merged = ner.merge_morph_from_token_origins(yap_morph, origins, validate_to_single=True).groupby('SentNum')['Label'].agg(list).to_list()
+
+    pred_multi = ner.evaluate_token_ner_nested(merged, tok_grouped)
+    
+    print('\nHYBRID + GOLD MULTI')
+    
+    yap_morph = ner.read_file_to_sentences_df(f'{ENV.ABSOLUTE_PATH_HEBREW_NER}/ncrf_results/transformer/morph/test-gold_multi_results.txt')
+    origins = ner.read_token_origins_to_df(f'{ENV.ABSOLUTE_PATH_HEBREW_NER}/utils_eval_files/yap_hybrid_gold_multi_test_tokens.txt')
+    
+    merged = ner.merge_morph_from_token_origins(yap_morph, origins, validate_to_single=True)
+    
+    merged = merged.groupby('SentNum')['Label'].agg(list).to_list()
+
+    gold_multi = ner.evaluate_token_ner_nested(merged, tok_grouped)
+    
+    return gold_morph_to_morph, gold_morph, pure_yap, pred_multi, gold_multi
+
 if __name__ == '__main__':
-    eval_all_trn_morph_dev()
+    eval_all_trn_morph_test()
--- utils/eval/make_yap_hybrid_trn_f.py
+++ utils/eval/make_yap_hybrid_trn_f.py
@@ -1,9 +1,10 @@
 from utils import ner, yap
-from utils.eval.consts import DEV
+from utils.eval.consts import DEV, TEST
 from utils.yap_graph import prune_lattices
 from app_env import ENV
 
-if __name__ == '__main__':
+
+def make_dev():
     tok = ner.read_file_to_sentences_df(DEV.TOK)
     
     multi = ner.read_file_to_sentences_df(DEV.MULTI)
@@ -45,3 +46,49 @@ if __name__ == '__main__':
 
     with open('utils_eval_files/yap_hybrid_trn_pred_multi_dev.txt', 'w') as w:
         w.write('\n\n'.join(md.groupby('SENTNUM')['FORM'].agg('\n'.join)))
+        
+def make_test():
+    tok = ner.read_file_to_sentences_df(TEST.TOK)
+    
+    multi = ner.read_file_to_sentences_df(TEST.MULTI)
+    
+    pred_multi = ner.read_file_to_sentences_df(f'{ENV.ABSOLUTE_PATH_HEBREW_NER}/ncrf_results/transformer/token_multi/test-results.txt')
+        
+    tok_str = ner.raw_toks_str_from_ner_df(tok)
+    
+    print("Gold morph")
+
+    ma = yap.yap_ma_api(tok_str)
+    
+    pruned = prune_lattices(ma, multi)
+    
+    md = yap.yap_joint_from_lattice_api(pruned)
+    
+    md['TOKEN'] = md['TOKEN'].astype(str)
+    
+    with open('utils_eval_files/yap_hybrid_trn_gold_multi_test_tokens.txt', 'w') as w:
+        w.write('\n\n'.join(md.groupby('SENTNUM')['TOKEN'].agg('\n'.join)))
+    
+    md['FORM'] = md['FORM'].apply(lambda x: x + ' O')
+
+    with open('utils_eval_files/yap_hybrid_trn_gold_multi_test.txt', 'w') as w:
+        w.write('\n\n'.join(md.groupby('SENTNUM')['FORM'].agg('\n'.join)))
+
+    print("Pred morph")
+    
+    pruned = prune_lattices(ma, pred_multi)
+    
+    md = yap.yap_joint_from_lattice_api(pruned)
+    
+    md['TOKEN'] = md['TOKEN'].astype(str)
+    
+    with open('utils_eval_files/yap_hybrid_trn_pred_multi_test_tokens.txt', 'w') as w:
+        w.write('\n\n'.join(md.groupby('SENTNUM')['TOKEN'].agg('\n'.join)))
+    
+    md['FORM'] = md['FORM'].apply(lambda x: x + ' O')
+
+    with open('utils_eval_files/yap_hybrid_trn_pred_multi_test.txt', 'w') as w:
+        w.write('\n\n'.join(md.groupby('SENTNUM')['FORM'].agg('\n'.join)))
+
+if __name__ == '__main__':
+    make_test()
