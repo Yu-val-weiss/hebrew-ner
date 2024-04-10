a6da02163bf6ec3519003e9d6ed36c756ac258a8
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-04-10 18:46:06 +0100

    test eval

--- utils/eval/eval_single.py
+++ utils/eval/eval_single.py
@@ -7,5 +7,11 @@ def eval_single_dev():
     single = ner.read_file_to_sentences_df(consts.DEV.TOK)
     return ner.evaluate_token_ner(pred_single['Label'].to_list(), single['Label'].to_list())
 
+def eval_single_test():
+    print('Single to single')
+    pred_single = ner.read_file_to_sentences_df('ncrf_results/tok-single/final/test-results.txt')
+    single = ner.read_file_to_sentences_df(consts.TEST.TOK)
+    return ner.evaluate_token_ner(pred_single['Label'].to_list(), single['Label'].to_list())
+
 if __name__ == '__main__':
     eval_single_dev()
