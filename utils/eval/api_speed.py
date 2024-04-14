50eba26ba024d7cc00370b96ff57a86d811b3cbb
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-04-14 13:32:04 +0100

    test speed

--- utils/eval/api_speed.py
+++ utils/eval/api_speed.py
@@ -2,11 +2,34 @@ import requests
 from random import choice
 import utils.ner as ner
 import conf
+import statistics
+from tqdm import tqdm
 
-def make_req(df):
-    max_sent_num = max(df['SentNum'])
-    rand_sent = choice(list(range(max_sent_num + 1)))
-    
+# mean: 228.97842693328857ms
+# stdev: 223.86241969210678ms   
 
 if __name__ == '__main__':
-    ner.read_file_to_sentences_df(conf.TEST)
+    tok = ner.read_file_to_sentences_df(conf.DEV.TOK)
+    
+    g = tok.groupby('SentNum')['Word'].agg(list)
+    
+    
+    times = []
+
+    for i in tqdm(range(1000)):
+        sent = ' '.join(choice(g))
+        r = requests.post(
+            'http://127.0.0.1:5000/predict',
+            json={
+                "text": sent,
+                "model": "hybrid",
+            }
+        )
+        
+        r.raise_for_status()
+        
+        times.append(float(r.headers['x-process-time-ms']))
+        
+    
+    print('mean:', statistics.mean(times))
+    print('stdev:', statistics.stdev(times))
