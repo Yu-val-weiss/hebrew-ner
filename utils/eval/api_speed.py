198b0a534cbd1082ac38d723d1e06db7840a3eab
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-04-16 17:25:41 +0100

    api speed

--- utils/eval/api_speed.py
+++ utils/eval/api_speed.py
@@ -1,22 +1,24 @@
+from typing import Dict
 import requests
 from random import choice
 import utils.ner as ner
 import config
-import statistics
+from scipy import stats
+import numpy as np
 from tqdm import tqdm
+import json
 
-# mean: 228.97842693328857ms
-# stdev: 223.86241969210678ms   
-
-if __name__ == '__main__':
+def speed_test(N = 500):
     tok = ner.read_file_to_sentences_df(config.DEV.TOK)
     
     g = tok.groupby('SentNum')['Word'].agg(list)
     
+    results: Dict = {
+        'N': N
+    }
     
     times = []
-
-    for i in tqdm(range(1000)):
+    for _ in tqdm(range(N), desc='Hybrid calls'):
         sent = ' '.join(choice(g))
         r = requests.post(
             'http://127.0.0.1:5000/predict',
@@ -29,7 +31,55 @@ if __name__ == '__main__':
         r.raise_for_status()
         
         times.append(float(r.headers['x-process-time-ms']))
-        
     
-    print('mean:', statistics.mean(times))
-    print('stdev:', statistics.stdev(times))
+        
+    print('hybrid')
+    print('min,max', np.min(times), np.max(times))
+    print('mean:', mean := np.mean(times))
+    print('stdev:', std := np.std(times))
+    print('conf interval:', conf := stats.norm.interval(0.95, loc=mean, scale=std / np.sqrt(N)))
+    
+    results['hybrid'] = {
+        'mean': mean,
+        'stdev': std,
+        'confidence_interval': conf,
+        'plusminus': mean - conf[0],
+    }
+    
+    times = []
+    for i in tqdm(range(N), desc='Standard calls'):
+        sent = ' '.join(choice(g))
+        r = requests.post(
+            'http://127.0.0.1:5000/predict',
+            json={
+                "text": sent,
+                "model": "token_single",
+            }
+        )
+        
+        r.raise_for_status()
+        
+        times.append(float(r.headers['x-process-time-ms']))
+    
+        
+    print('token-single')
+    print('min,max', np.min(times), np.max(times))
+    print('mean:', mean := np.mean(times))
+    print('stdev:', std := np.std(times))
+    print('conf interval:', conf := stats.norm.interval(0.95, loc=mean, scale=std / np.sqrt(N)))
+    
+    results['token-single'] = {
+        'mean': mean,
+        'stdev': std,
+        'confidence_interval': conf,
+        'plusminus': mean - conf[0],
+    }
+    
+    print(results)
+    
+    with open('utils/eval/api_speed_result.json', 'w') as f:
+        json.dump(results, f, indent=4)
+
+if __name__ == '__main__':
+    speed_test(500)
+    
